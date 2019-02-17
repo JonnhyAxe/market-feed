0d3c001c6258026ba0dde4ec4bb0b7d0261afb8c
JonnhyAxe <joao_m_s_m@hotmail.com>
2019-02-17 22:33:15 +0000

    Revert "MF: install vue-rx"
    This reverts commit 20549123a7b30479211cc9b87d81baa23a917ef0.

--- frontend-feed/src/router/index.js
+++ frontend-feed/src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import PerfectMatch from '@/components/PerfectMatch'
 import RichGridExample from '@/rich-grid-example/RichGridExample.vue'
 
 Vue.use(Router)
@@ -9,26 +7,11 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/tmp',
-      name: 'PerfectMatch',
-      component: PerfectMatch
-    },
-    {
       path: '/',
       components: {
           default: RichGridExample
       },
       name: "Rich Grid with Pure JavaScript"
-    },
-    {
-      path: '/rx',
-      name: 'rxPerfectMatch',
-      component: PerfectMatch
     }
   ]
 })
